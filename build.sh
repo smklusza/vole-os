55cd22dada9e580d6709da9eddb1af08293c03ca
Stephen Klusza <smklusza@gmail.com>
2024-10-16 20:56:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,28 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+https://download.docker.com/linux/fedora/docker-ce.repo
 # this installs a package from fedora repos
 rpm-ostree install screen
+rpm-ostree install firewalld
+rpm-ostree install manafirewall
+rpm-ostree install usbguard
+rpm-ostree install usbguard-notifier
+rpm-ostree install samba
+rpm-ostree install podman
+rpm-ostree install podman-compose
+rpm-ostree install containers-common-extra
+rpm-ostree install docker-ce
+rpm-ostree install docker-ce-cli
+rpm-ostree install containerd.io
+rpm-ostree install docker-compose-plugin
+rpm-ostree install numix-icon-theme-circle
+rpm-ostree install ulauncher
+rpm-ostree install plank
+rpm-ostree install gtk-xfce-engine
+rpm-ostree install flat-remix-gtk2-theme
+rpm-ostree install pop-gtk2-theme
+rpm-ostree install pop-icon-theme
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -21,3 +40,9 @@ rpm-ostree install screen
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable podman.service
+systemctl enable usbguard-notifier.service
+systemctl enable smb.service
+systemctl enable smb.socket
+systemctl enable docker.socket
+systemctl enable docker.service
